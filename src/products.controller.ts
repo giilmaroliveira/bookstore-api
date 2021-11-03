50d682ca8537a7a3a7431b65a5d5e760688a622f
Gilmar Oliveira <giilmaroliveira@outlook.com>
2021-11-03 15:39:28 -0300

    Modeling product and changing the return type data

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -7,33 +7,39 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('BOOK001', 'TDD e BDD na prática', 29.9),
+    new Product('BOOK002', 'Iniciando com Flutter', 39.9),
+    new Product('BOOK003', 'Inteligência artificial como serviço', 29.9),
+  ];
+
   @Get()
-  getAll(): string {
-    return 'All products';
+  getAll(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  getById(@Param() params): string {
-    return `Get product by id ${params.id}`;
+  getById(@Param() params): Product {
+    return this.products[0];
   }
 
   @Post()
-  post(@Body() body): string {
-    console.log(body);
-    return 'Create a new product';
+  post(@Body() product: Product): void {
+    product.id = 1;
+    this.products.push(product);
   }
 
   @Put(':id')
-  put(@Body() body, @Param() params): string {
-    console.log(body);
-    return `Update the product ${params.id}`;
+  put(@Body() product: Product, @Param() params): Product {
+    return product;
   }
 
   @Delete(':id')
-  delete(@Param() params): string {
-    return `Delete the product ${params.id}`;
+  delete(@Param() params): void {
+    this.products.pop();
   }
 }
